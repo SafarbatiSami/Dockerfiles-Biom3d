02a7b2f58296d71b2260c41312db27c4022813d6
Sami SAFARBATI <114927265+SafarbatiSami@users.noreply.github.com>
2024-10-14 14:24:06 +0200

    Add files via upload

--- v7/entrypoint.sh
+++ v7/entrypoint.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+
+# Debugging: Print current directory and environment variables
+
+# Ensure at least one argument is provided
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 <python_module> [params...]"
+    exit 1
+fi
+
+# Extract the Python module name
+python_module="$1"
+shift  # Remove the first argument to leave only the parameters
+
+# Run the specified Python module with remaining parameters
+exec python3 -m "$python_module" "$@"
 
