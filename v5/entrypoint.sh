4d5d3e0bd9776e0d14fcc77dae18d168c768ccbf
Sami SAFARBATI <114927265+SafarbatiSami@users.noreply.github.com>
2024-10-14 14:22:23 +0200

    Add files via upload

--- v5/entrypoint.sh
+++ v5/entrypoint.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
+
+# Activate the conda environment
+source /opt/conda/bin/activate pytorch_env
+
+
+# Ensure at least one argument is provided
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 <python_module> [params...]"
+    exit 1
+fi
+
+# Extract the Python module name
+python_module="$1"
+shift  # Remove the first argument to leave only the parameters
+
+# Run the specified Python module with remaining parameters
+exec python3 -m "$python_module" "$@"
 
